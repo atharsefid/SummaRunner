04f2a0a3e6dc1f484a89a939f041364ae4310f20
Athar Sefid <azs5955@psu.edu>
2020-05-30 18:29:03 -0400

    Update batch_model.py

--- batch_model.py
+++ batch_model.py
@@ -40,40 +40,31 @@ class SummaRuNNer(object):
             print('flat word embeded:', document_word_embedding)
             #documents_word_embedding = tf.reshape(document_word_embedding, [-1, self.doc_len, 
             #                                                              self.sent_len, self.embedding_size])
-            #print(documents_word_embedding)
+            
             all_sents_word_embedding = tf.reshape(document_word_embedding, [-1, self.sent_len, self.embedding_size])
             # shape: batch_size*doc_len, sent_len, embed_size
             # now it is 3 dimensional and suitable for applying GRU
-            # shape=(6400, 50, 50)
-            print('********* before gru: ',all_sents_word_embedding)
     
         with tf.variable_scope("sent_level_BiGRU"):
             fw_cell = tf.nn.rnn_cell.GRUCell(self.hidden_size)
             bw_cell = tf.nn.rnn_cell.GRUCell(self.hidden_size)
             self.sent_GRU_out, _ = tf.nn.bidirectional_dynamic_rnn(fw_cell, bw_cell, all_sents_word_embedding, scope='bi-GRU',
                                                                    dtype=tf.float32)
-            self.sent_bigru = tf.concat([self.sent_GRU_out[0], self.sent_GRU_out[1]], 2) #  shape=(6400, 50, 400)  
-            self.sent_bigru_avg = tf.reduce_mean(self.sent_bigru, axis=1) # shape=(6400, 400)
-            print('********* reduce mean out: ',self.sent_bigru_avg) 
+            self.sent_bigru = tf.concat([self.sent_GRU_out[0], self.sent_GRU_out[1]], 2) 
+            self.sent_bigru_avg = tf.reduce_mean(self.sent_bigru, axis=1) 
             self.doc_sent_embed = tf.reshape(self.sent_bigru_avg, (params.batch_size, self.doc_len , 2 * self.hidden_size))
-            print('********* doc_sent_embed : ',self.doc_sent_embed)  # shape=(64, 100, 400)
             
         with tf.variable_scope("doc_level_BiGRU"):
             fw_cell_2 = tf.nn.rnn_cell.GRUCell(self.hidden_size)
             bw_cell_2 = tf.nn.rnn_cell.GRUCell(self.hidden_size)
             doc_GRU_out, _ = tf.nn.bidirectional_dynamic_rnn(fw_cell_2, bw_cell_2, self.doc_sent_embed,
                                                              dtype=tf.float32)
-            self.doc_bigru = tf.concat([doc_GRU_out[0], doc_GRU_out[1]], 2) # shape=(64, 100, 400)
-            print('********* doc bigru : ',self.doc_bigru)
-            self.docs = tf.reduce_mean(self.doc_bigru, axis=1) # shape=(64, 400)
-            print('********* docs : ',self.docs) 
-            
-            
+            self.doc_bigru = tf.concat([doc_GRU_out[0], doc_GRU_out[1]], 2) 
+            self.docs = tf.reduce_mean(self.doc_bigru, axis=1)         
             # document embedding
             Wf0 = tf.Variable(tf.random.uniform([2 * self.hidden_size, 100], -1.0, 1.0), name='W0')
             bf0 = tf.Variable(tf.zeros(shape=[100]), name='b0')
-            self.docs_embeds = tf.nn.relu(tf.matmul(self.docs, Wf0) + bf0) # shape=(64, 100)
-            print('********* docs_embeds:', self.docs_embeds)
+            self.docs_embeds = tf.nn.relu(tf.matmul(self.docs, Wf0) + bf0) 
 
             # position embedding
             Wpe = tf.Variable(tf.random.normal([500, self.sent_len]))
@@ -91,33 +82,23 @@ class SummaRuNNer(object):
         with tf.variable_scope("score_layer"):
 
             for position, sent_hidden in enumerate(tf.unstack(self.doc_bigru, axis=(1))):
-                #print('********* sent hidden', sent_hidden) # shape=(64, 400) # The first sentences of all docs
-                sy = tf.nn.relu(tf.matmul(sent_hidden, Wf) + bf) # shape= (64,100)
-                #print('********* sy:',sy) 
-                h = tf.transpose(sy, perm=(1, 0)) # shape=(100,64)
-                pos_embed = tf.nn.embedding_lookup(Wpe, position) # shape=(50,)
-                #print('********* pos_embed: ', pos_embed)
-                p = tf.reshape(pos_embed, (1, -1)) # shape=(50,1)
-                positions = tf.tile(p, tf.constant([params.batch_size,1])) # shape=(64, 50)
-                #print('********* positions: ', positions)
-                content = tf.squeeze(tf.matmul(Wc, h)) # shape(64,)
-                #print('********* content:', content)
-                salience = tf.reduce_sum(tf.multiply(tf.matmul(sy,Ws), self.docs_embeds), axis=1) # shape=(64,)
-                #print('********* salience: ',salience)
-                novelty = -1 * tf.reduce_sum(tf.multiply(tf.matmul(sy,Wr), tf.tanh(self.docs_embeds)), axis=1) # shape=(64,)
-                #print('********* novelty: ', novelty)
-                position_embeds = tf.reduce_sum(tf.multiply(Wp,positions), axis=1) # shape=(64,)
-                #print('********* position_embeds: ', position_embeds)
-
-                Prob = tf.sigmoid(content + salience + novelty + position_embeds + bias) # shape=(64,)
-                #print('********* prob: ', Prob)
-                #s = s + tf.multiply(h, Prob)
-                multiplier = tf.tile(tf.reshape(Prob,[-1,1]),tf.constant([1,100], tf.int32)) # shape=(64,100)
-                #print('********* multiplier: ', multiplier)
+                #print('********* sent hidden', sent_hidden)  # The first sentences of all docs
+                sy = tf.nn.relu(tf.matmul(sent_hidden, Wf) + bf)
+                h = tf.transpose(sy, perm=(1, 0)) 
+                pos_embed = tf.nn.embedding_lookup(Wpe, position) 
+                p = tf.reshape(pos_embed, (1, -1)) 
+                positions = tf.tile(p, tf.constant([params.batch_size,1]))
+                content = tf.squeeze(tf.matmul(Wc, h)) 
+                salience = tf.reduce_sum(tf.multiply(tf.matmul(sy,Ws), self.docs_embeds), axis=1)
+                novelty = -1 * tf.reduce_sum(tf.multiply(tf.matmul(sy,Wr), tf.tanh(self.docs_embeds)), axis=1)            
+                position_embeds = tf.reduce_sum(tf.multiply(Wp,positions), axis=1) 
+                Prob = tf.sigmoid(content + salience + novelty + position_embeds + bias) 
+                multiplier = tf.tile(tf.reshape(Prob,[-1,1]),tf.constant([1,100], tf.int32))
                 s = s + tf.multiply(multiplier, sy) 
                 scores.append(Prob)
-            self.y_ = tf.transpose(tf.convert_to_tensor(scores, name="prediction"), perm=(1, 0)) # shape=(64,100)
-            print('********* y_', self.y_)
+            self.y_ = tf.transpose(tf.convert_to_tensor(scores, name="prediction"), perm=(1, 0)) 
+          
+                
         # Calculate Mean cross-entropy loss
         with tf.name_scope("loss"):
 
@@ -127,8 +108,7 @@ class SummaRuNNer(object):
             output = self.y_        
             self.loss = tf.reduce_sum(
                  -(target * tf.log(output + epsilon_one) + (1. - target) * tf.log(1. - output + epsilon_zero)))
-            print('******loss: ', self.loss)
             #weight = 0.7
             #self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=target, logits=output))
-            # setting the pos_weight increases the cost if a positive saple is misclassified vs misclassification of a negative sample
+            # setting the pos_weight increases the loss if a positive sample is misclassified vs misclassification of a negative sample
             #self.loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(labels=target, logits=output, pos_weight=20))
